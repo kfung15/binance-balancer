754724c8a9ca769d57a5ef88ff80cb41b390b82b
Ken Fung <kennardfung01@gmail.com>
2020-12-11 19:04:55 +0800

    data updater and risk manager initial commit

---
+++ data_updater.R
@@ -0,0 +1,117 @@
+# Name: Kennard Fung
+# Project Title: data_updater.R
+# Project Description: Grabs historical crypto data from Binance. Creates data files if they
+#                      do not exist.
+
+library(PerformanceAnalytics)
+library(httr)
+
+cryptos_of_interest = c("BNB", "BTC", "XLM", "XMR", "XRP", "MANA", "ENJ", "RVN")
+
+# cd to the folder that contains price information. If none is available, create one and populate
+
+setwd("/Users/kenfung/github/binance-balancer-R/crypto_data/")
+for(crypto in cryptos_of_interest){
+  # We're gonna use USDT instead of BUSD because of the greater amount of data available
+  usdt_name_transform = paste(crypto,"USDT_prices.csv",sep="")
+  if(!(file.exists(usdt_name_transform))){
+    # At this point, the file does not exist for a certain crypto
+    print(paste("Weekly data files not available for ", crypto, "USDT, creating now..."),sep="")
+    data_fully_extracted = FALSE
+    price.mat = matrix(nrow=0,ncol=2)
+    earliest_time = as.numeric(as.POSIXct(Sys.time())) * 1000;
+    while(data_fully_extracted == FALSE){
+      query = list(symbol=paste(crypto,"USDT",sep=""),interval="1w",limit=1000,endTime=ceiling(as.numeric(earliest_time)))
+      resp = httr::GET("https://api.binance.com/api/v3/klines",query=query)
+      if(http_error(resp) == TRUE){
+        # Something went wrong with the request
+        print("Something went wrong!")
+        next
+      }
+
+      jsonRespText = content(resp,as="text")
+      print(nrow(as.matrix(fromJSON(jsonRespText))))
+      if(length(fromJSON(jsonRespText)) > 0){
+        temp_price.mat = as.matrix(fromJSON(jsonRespText)[,5])
+        temp_price.mat = cbind(as.matrix(fromJSON(jsonRespText)[,1]),temp_price.mat)
+        price.mat = rbind(temp_price.mat,price.mat)
+      }
+      
+      if(nrow(as.matrix(fromJSON(jsonRespText))) < 1000){
+        print("No more data left!")
+        data_fully_extracted = TRUE
+      } else {
+        print("There is some data still available!")
+        earliest_time = as.numeric(as.matrix(fromJSON(jsonRespText))[1,1]) - 86400000
+      }
+    }
+    
+    # At this point, you have price.mat which contains the full price data
+    # Chop off the last thing, because it hasn't reached the endTime yet
+    price.mat = as.matrix(price.mat[1:(nrow(price.mat)-1),])
+    price.z = zoo(price.mat,order.by = index(price.mat))
+    write.csv(as.data.frame(price.z),paste(crypto,"USDT_prices.csv",sep=""))
+    returns.z = diff(log(as.numeric(price.z[,2])))
+    write.csv(as.data.frame(returns.z),paste(crypto,"USDT_returns.csv",sep=""))
+  } else {
+    # Update the csv folder with the latest data point
+
+    earliest_time = as.numeric(as.POSIXct(Sys.time())) * 1000;
+    query = list(symbol=paste(crypto,"USDT",sep=""),interval="1w",limit=1000,endTime=ceiling(as.numeric(earliest_time)))
+    resp = httr::GET("https://api.binance.com/api/v3/klines",query=query)
+    
+    jsonRespText = content(resp,as="text")
+    
+    if(http_error(resp) == TRUE){
+      # Something went wrong with the request
+      print("Something went wrong!")
+    }
+
+    latest_time = as.numeric(fromJSON(jsonRespText)[nrow(fromJSON(jsonRespText)),1])
+    penultimate_time = as.numeric(fromJSON(jsonRespText)[(nrow(fromJSON(jsonRespText)) - 1),1])
+    penultimate_price = as.numeric(fromJSON(jsonRespText)[(nrow(fromJSON(jsonRespText)) - 1),5])
+    
+    # Import the price.csv file
+    price.csv = read.csv(paste(crypto,"USDT_prices.csv",sep = ""))
+    #Remove the first column
+    price.csv = price.csv[,2:3]
+
+    # Only update if the week has actually passed. Otherwise you'd just be making a double ending entry.
+    if(penultimate_time > price.csv[nrow(price.csv),1]) {
+      temp_update = c()
+      temp_update = c(temp_update,penultimate_time)
+      temp_update = c(temp_update,penultimate_price)
+      temp_update = t(as.data.frame(temp_update))
+      colnames(temp_update) = colnames(price.csv)
+      rownames(temp_update) = as.character(as.numeric(rownames(price.csv[nrow(price.csv),])) + 1)
+      price.csv = rbind(price.csv,temp_update)
+      
+      # Export the price file
+      write.csv(price.csv,paste(crypto,"USDT_prices.csv"))
+      
+      # Import the returns file
+      returns.csv = read.csv(paste(crypto,"USDT_returns.csv",sep = ""))
+      
+      # Get rid of the first column
+      returns.csv = returns.csv[,2]
+      
+      # Add the new cc returns data point
+      last_two_points = c()
+      last_two_points = c(last_two_points,as.numeric(fromJSON(jsonRespText)[(nrow(fromJSON(jsonRespText)) - 2),5]))
+      last_two_points = c(last_two_points,penultimate_price)
+      delta = diff(log(last_two_points))
+      
+      returns.csv = c(returns.csv,delta)
+      
+      # Export the returns file
+      write.csv(returns.csv,paste(crypto,"USDT_returns.csv",sep=""))
+      
+    } else {
+      print("It's not time to update yet!")
+    }
+    
+  }
+  
+}
+
+
---
+++ risk_manager.R
@@ -0,0 +1,108 @@
+# Name: Kennard Fung
+# Project Title: risk_manager.R
+# Project Description: Given historical price data, this program creates an optimal portfolio
+#                      based on a user's risk preference.
+
+library(PerformanceAnalytics)
+library(jsonlite)
+
+# Uncomment and run this first if you don't have a weights.csv file yet
+# cryptos_of_interest = as.matrix(c("XLM", "BTC", "XRP", "ENJ"))
+# weights = as.matrix(c(0.05,0.05,0.05,0.05))
+# cryptos_of_interest = cbind(cryptos_of_interest,weights)
+# colnames(cryptos_of_interest) = c("Crypto", "Weight")
+# write.csv(cryptos_of_interest,"weights.csv")
+# ---------------------------------------------------------
+
+var_pvalue = 0.95
+risk_tolerance = 0.15
+portfolios_to_create = 100000
+crypto_list_size = 4
+cryptos_of_interest = c("XLM", "BTC", "XRP", "ENJ")
+
+
+# First, upload the weights file
+weights = read.csv("weights.csv")
+weights = weights[,2:3]
+confirm_array = c()
+weight_array = c()
+for(x in 1:length(cryptos_of_interest)){
+  for(y in 1:nrow(weights)){
+    if(cryptos_of_interest[x] == as.character(weights[y,1])) {
+      confirm_array = c(confirm_array,as.character(weights[y,1]))
+      weight_array = c(weight_array,weights[y,2])
+      next
+    } 
+  }
+}
+
+if(is.na(match(FALSE,(confirm_array == cryptos_of_interest)))){
+  # Only continue if both arrays of crypto names match!
+  var_list = c()
+  for(x in 1:length(cryptos_of_interest)){
+    # Upload the cc returns file
+    returns = read.csv(paste(crypto,"USDT_returns.csv",sep=""))
+    returns = returns[,2]
+    
+    # Collect the VaR for the particular crypto's returns
+    var = VaR(returns,p=var_pvalue,method=c("historical"),invert=FALSE)
+    var_list = c(var_list,var)
+  }
+  
+  # At this point, you have a set of VaRs
+  # If the current risk profile is within +/- 5% of the risk_tolerance, then do nothing.
+  risk_profile = var_list * weight_array
+  if(abs(sum(var_list * weight_array) - risk_tolerance) > (0.05*(risk_tolerance))){
+    # If not, create a million portfolios with asset weights between 5% and 10%
+    # Sum the absolute differences in weights for each asset. Keep the portfolio that most closely resembles the existing one 
+    # AND is within 5% of the risk_tolerance
+    
+    best_portfolio_so_far = c()
+    lowest_sum_distance = 99999.999
+    lowest_risk_difference = 99999.999
+    
+    for(x in 1:portfolios_to_create){
+      print(paste(x, "Risk:", lowest_risk_difference, "Sum Difference:", lowest_sum_distance, sep = " "))
+      sample_portfolio = c()
+      # Populate the sample portfolio with random weights
+      for(y in 1:crypto_list_size){
+        sample_portfolio = c(sample_portfolio,runif(1,0.10,0.15))
+      }
+      
+      # Calculate the overall portfolio risk and proceed only if the sum is within +/-5%
+      # of the risk_tolerance
+      
+      if(abs(sum(sample_portfolio * var_list) - risk_tolerance) < (0.05*risk_tolerance)){
+        temp_sum_distance = sum(abs(weight_array - sample_portfolio))
+        if(temp_sum_distance < lowest_sum_distance & (abs(sum(sample_portfolio * var_list) - risk_tolerance) < lowest_risk_difference)){
+          lowest_sum_distance = temp_sum_distance
+          lowest_risk_difference = abs(sum(sample_portfolio * var_list) - risk_tolerance)
+          best_portfolio_so_far = sample_portfolio
+        }
+      }
+    }
+  }
+  
+  # At this point, you have the best possible portfolio
+  # Update the weights file and generate the JSON file
+  
+  confirm_array = as.data.frame(confirm_array)
+  best_portfolio_so_far = as.data.frame(best_portfolio_so_far)
+  confirm_array = cbind(confirm_array,best_portfolio_so_far)
+  colnames(confirm_array) = c("Crypto", "Weight")
+  write.csv(confirm_array,"weights.csv")
+  write_json(confirm_array,"weights.json")
+
+} else {
+  print("Crypto array names do not match!")
+}
+
+
+
+
+
+
+
+
+
+
